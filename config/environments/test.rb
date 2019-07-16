bd8cc3d8f0dc212dd5e42fec3f01601f0017f009
Jack <superjackpower85@gmail.com>
2019-07-16 16:13:00 +0800

    third commit

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,15 +1,12 @@
 # Include the general initialization 
 require File.expand_path("../init", __FILE__)
-
 # Requiring the necessary gem for testing
 require 'rspec'
 require 'rack/test'
-
 APP_TEST_CONFIG = File.join(APP_ROOT, "config", "environments", "test")
 APP_MODELS = File.join(APP_ROOT, "app", "models")
 APP_CONTROLLERS = File.join(APP_ROOT, "app", "controllers")
 APP_VIEWS = File.join(APP_ROOT, "app", "views")
-
 def app
 	Sinatra::Application
 end
