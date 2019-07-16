6aeec251bcd959bc490edad58dfb16d89c996d71
Jack <superjackpower85@gmail.com>
2019-07-16 15:56:14 +0800

    initial commit

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,13 +5,11 @@ require File.expand_path("../init", __FILE__)
 require 'rspec'
 require 'rack/test'
 
-# Creating constant pathname
 APP_TEST_CONFIG = File.join(APP_ROOT, "config", "environments", "test")
 APP_MODELS = File.join(APP_ROOT, "app", "models")
 APP_CONTROLLERS = File.join(APP_ROOT, "app", "controllers")
 APP_VIEWS = File.join(APP_ROOT, "app", "views")
 
-# Defining app as Sinatra application
 def app
 	Sinatra::Application
 end
